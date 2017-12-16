a4ef7c0202c58429c6e2f6ca52cedf483b9e913b
Fernando Bellincanta <ervalhous@hotmail.com>
2017-12-16 13:05:02 -0300

    More slides per product and some polishing

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,8 @@
 Spree::Product.class_eval do
-  has_one :slide, dependent: :destroy
-  after_update :destroy_slide_if_deleted
+  has_many :slides, dependent: :destroy
+  after_update :destroy_slides_if_deleted
 
-  def destroy_slide_if_deleted
-    slide.update_attributes(published: false) if slide && deleted_at
+  def destroy_slides_if_deleted
+    slides.map{|slide| slide.update_attributes(published: false)} if slides && deleted_at
   end
 end
